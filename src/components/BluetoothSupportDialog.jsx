4b76b450959a0102454c32193f861c610beb36fd
oscargavin <oscarfgavin@gmail.com>
2024-12-22 13:13:19 +0000

    dialog fix

--- src/components/BluetoothSupportDialog.jsx
+++ src/components/BluetoothSupportDialog.jsx
@@ -8,7 +8,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+} from "../components/ui/alert-dialog";
 import { Info, ExternalLink } from 'lucide-react';
 import { detectEnvironment } from '../utils/environment';
 
